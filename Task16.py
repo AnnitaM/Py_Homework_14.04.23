7560e5342d3b3c0b9fc8cd6754f250ba77aa6d6c
Annita Maniri <annitk@gmail.com>
2023-04-14 12:31:58 +0300

    add Task16

---
+++ Task16.py
@@ -0,0 +1,15 @@
+# Требуется вычислить, сколько раз встречается некоторое число X в массиве A[1..N].
+# Пользователь в первой строке вводит натуральное число N – количество элементов в массиве.
+# В последующих  строках записаны N целых чисел Ai. Последняя строка содержит число X
+# *Пример:*
+# 5
+#     1 2 3 4 5
+#     3
+#     -> 1
+
+nums_elem = int(input("Enter the number of elemens in array: "))
+array = input().split()
+unkn_num = input("Enter the unknown number: ")
+print(array)
+print(f"The number {unkn_num} occurs in array {array.count(unkn_num)} times")
+
